7679b03a0f17008d07b0f0822bc529d172175cde
Ahmed-Moin-619 <143007266+Ahmed-Moin-619@users.noreply.github.com>
2024-06-19 13:19:05 +0000

    DataTypes

---
+++ .devcontainer/01_Basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"  // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+let name = "moin"
+let age = 23
+let issLoggedIn = false
+
+/* Data Types
+Perimitive Data Types--------
+
+String => ""
+Number => 2 to power 53
+Bigint => Like Number But uses largest Number
+Boolean => true/false
+Undefined => undefined Means variable declare but value not assigned
+Null => standalone value means the value is empty
+Symbol => use of uniqueness
+
+Non Perimitive Data Types-------
+object
+*/
+
+//To know the type of any variable
+console.log(typeof null); // type object
+console.log(typeof undefined); // type undefined
+
+
