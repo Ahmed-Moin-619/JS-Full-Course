9a1a7f2db4c3d232a2a5aa7142d266abd0b92c20
Ahmed-Moin-619 <143007266+Ahmed-Moin-619@users.noreply.github.com>
2024-06-27 13:07:52 +0000

    Dates in JavaScript

---
+++ .devcontainer/01_Basics/07_datesin.js
@@ -0,0 +1,31 @@
+// Dates
+
+let myDates = new Date()
+// console.log(myDates)
+// console.log(myDates.toString())
+// console.log(myDates.toDateString())
+// console.log(myDates.toTimeString())
+// console.log(myDates.getMonth())
+// console.log(myDates.getDay())
+// console.log(typeof myDates)
+
+// let myCreatedDates = new Date(2023, 0, 23)
+// let myCreatedDates = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDates = new Date("2023-01-14")
+let myCreatedDates = new Date("01-14-2023")
+// console.log(myCreatedDates.toLocaleString())
+
+let myTimeStamps = Date.now()
+// console.log(myTimeStamps)
+// console.log(myCreatedDates.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+
+// let newDate = new Date()
+// console.log(newDate)
+
+// newDate.toLocaleString('default',{
+//     weekday: "long",
+    
+// })
+
